50d8917394b1104109990510a1f60e232c7767b7
Tom Smith <Tom_Esq@Yahoo.com>
2024-07-26 17:15:06 +0100

    Added base project for what av got so far

---
+++ RSAEncrypter/RSAEncrypter/Helpers/PrimeHelper.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace RSAEncrypter.Helpers;
+public static class PrimeHelper
+{
+    public static IEnumerable<BigInteger> CalculateCoprimes(BigInteger p, BigInteger q)
+    {
+
+
+
+
+        
+
+        return [];
+    }
+
+    public static IEnumerable<BigInteger> CalculateNPrimes_Fermat(int primeCount)
+    {
+        var result = new List<BigInteger>();
+
+        var sw = new Stopwatch();
+        
+        int i = 1;
+        long totalElapsedMS = 0;
+
+        while (result.Count < primeCount)
+        {
+            sw.Reset();
+            sw.Start();
+
+            for (int j = 1; j < i; j++)
+            {
+                if (BigInteger.DivRem(BigInteger.Pow(j, i - 1), i).Remainder != 1)
+                    break;
+
+                if(j == i - 1)
+                {
+
+                    Console.WriteLine($"Found prime[{result.Count} / {primeCount}]:  {i} in {sw.ElapsedMilliseconds}ms");
+                    totalElapsedMS += sw.ElapsedMilliseconds;
+
+                    result.Add(i);
+                }
+            }
+
+            sw.Stop();
+
+            i += 2;
+        }
+
+        Console.WriteLine($"Found {primeCount} prime numbers in {totalElapsedMS}ms with an average of {totalElapsedMS / primeCount}ms per prime resolve");
+
+        return result;
+    }
+
+    public static BigInteger CalculateTotient(BigInteger p, BigInteger q) => BigInteger.Multiply(p - 1, q - 1);
+
+    public static BigInteger Sqrt(this BigInteger n)
+    {
+        if (n == 0) return 0;
+        if (n > 0)
+        {
+            int bitLength = Convert.ToInt32(Math.Ceiling(BigInteger.Log(n, 2)));
+            BigInteger root = BigInteger.One << (bitLength / 2);
+
+            while (!isSqrt(n, root))
+            {
+                root += n / root;
+                root /= 2;
+            }
+
+            return root;
+        }
+
+        throw new ArithmeticException("NaN");
+    }
+
+    private static Boolean isSqrt(BigInteger n, BigInteger root)
+    {
+        BigInteger lowerBound = root * root;
+        BigInteger upperBound = (root + 1) * (root + 1);
+
+        return (n >= lowerBound && n < upperBound);
+    }
+}
---
+++ RSAEncrypter/RSAEncrypter/Program.cs
@@ -0,0 +1,39 @@
+﻿using RSAEncrypter.Helpers;
+using System.Numerics;
+using System.Text;
+
+var primeCount = 300;
+
+Console.WriteLine($"Calculating the first {primeCount} primes");
+
+_ = PrimeHelper.CalculateNPrimes_Fermat(primeCount);
+
+Console.ReadLine();
+
+
+
+void RunRSAEncryption()
+{
+    Console.WriteLine("RSA Encrypter\n");
+
+    int p = 7;
+    int q = 19;
+    var product = BigInteger.Multiply(p, q);
+    var totient = (p - 1) * (q - 1);
+    var publicKey = 29;
+    var privateKey = 41;
+
+    var message = "Remo";
+
+    int message_bits = BitConverter.ToInt32(Encoding.UTF8.GetBytes(message), 0);
+
+    var message_e = BigInteger.DivRem(BigInteger.Pow(message_bits, publicKey), product).Remainder;
+    var message_d = BigInteger.DivRem(BigInteger.Pow(message_e, privateKey), product).Remainder;
+
+    var decoded_message = Encoding.UTF8.GetString(message_d.ToByteArray());
+
+    Console.WriteLine($"Message : {message} | bits: {message_bits}");
+    Console.WriteLine($"Cipher : {message_e}");
+    Console.WriteLine($"Decrypted Cipher: {message_d}");
+    Console.WriteLine($"Decrypted Message : {decoded_message}");
+}
